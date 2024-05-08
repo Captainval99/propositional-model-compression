865e72125439014aefe934d7efbd68bb23f0efd8
Captainval99 <valentin.schenk@gmx.de>
2024-05-08 17:43:18 +0200

    change propagation to remove very expensive std::find call

--- src/propagation/Propagation.h
+++ src/propagation/Propagation.h
@@ -2,6 +2,7 @@
 #define SRC_PROPAGATION_PROPAGATION_H_
 
 #include <deque>
+#include <unordered_set>
 
 
 #include "SATTypes.h"
@@ -11,10 +12,12 @@ namespace Propagation {
     void propagate(std::vector<Cl>& clauses, std::vector<Var>& variables, Var propagationVar) {
         //queue for variables that are propagated
         std::deque<Var> propagationQueue = {propagationVar};
+        std::unordered_set<unsigned int> propagationQueueIds = {propagationVar.id}; 
 
         while (!propagationQueue.empty()) {
             Var var = propagationQueue.front();
             propagationQueue.pop_front();
+            propagationQueueIds.erase(var.id);
 
             //choose the right occurence list
             std::vector<Cl*> occList;
@@ -43,7 +46,7 @@ namespace Propagation {
 
                         if (unitVar.state == Assignment::OPEN) {
                             //check if variable is already in queue
-                            if (std::find(propagationQueue.begin(), propagationQueue.end(), unitVar) != std::end(propagationQueue)) {
+                            if (propagationQueueIds.count(unitVar.id)) {
                                 break;
                             }
 
@@ -54,6 +57,7 @@ namespace Propagation {
                             }
 
                             propagationQueue.push_back(unitVar);
+                            propagationQueueIds.insert(unitVar.id);
                             break;
                         }
                     }
